78dce682b16ba8866d8c09c1811d7460b25f2a34
HANNES PAULSSON <hannes.paulsson@gmail.com>
2016-02-23 10:45:40 +0100

    started with import project

--- app/src/main/java/nu/geeks/uio_kth/Activities/ImportProjectView.java
+++ app/src/main/java/nu/geeks/uio_kth/Activities/ImportProjectView.java
@@ -2,18 +2,43 @@ package nu.geeks.uio_kth.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import nu.geeks.uio_kth.R;
 
 /**
  * Created by hannespa on 16-02-23.
  */
 public class ImportProjectView extends Activity {
 
+    static final String TAG ="IMPORT_PROJECT";
+
+    TextView project_name;
+
+    String projectId;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_project);
 
-        Bundle b = getIntent().getExtras();
+        //Get the exact URL
+        //string structure: https://u.io/?id=xxxxxxx
+        String msg = getIntent().getDataString();
+        if(msg.contains("?id=")){
+            int pos = msg.indexOf("=");
+            projectId = msg.substring(pos+1);
+            Log.e(TAG, projectId);
+        }else{
+            Log.e(TAG, "UNPARSABLE URL");
+            finish();
+        }
+
+        project_name = (TextView) findViewById(R.id.tv_imp_proj_name);
+        project_name.setText(projectId);
 
     }
 }
