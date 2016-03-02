e19a77b4b29810b531df6e886cb67d95a2c131b8
miandre <miandre@kth.se>
2016-03-02 11:58:47 +0100

    Edited user class

--- app/src/main/java/nu/geeks/uio_kth/Objects/User.java
+++ app/src/main/java/nu/geeks/uio_kth/Objects/User.java
@@ -3,6 +3,7 @@ package nu.geeks.uio_kth.Objects;
 import android.support.annotation.NonNull;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,14 +17,26 @@ import java.util.TreeMap;
  */
 public class User {
 
-private static Map<String,Integer> names = new TreeMap<>();
+private static Map<String,Integer> names = new HashMap<>();
 
     public static void addName(String name){
-        int count = 0;
 
         if(names.containsKey(name)){
             names.put(name,names.get(name)+1);
         }else names.put(name,1);
+    }
+
+    public static String getName(){
+
+        String retName = Collections.max(names.entrySet(),new Comparator<Map.Entry<String,Integer>>(){
+
+            @Override
+            public int compare(Map.Entry<String, Integer> lhs, Map.Entry<String, Integer> rhs) {
+                return lhs.getValue()>rhs.getValue()? 1:-1;
+            }
+        }).getKey();
+
+        return retName;
     }
 
 
